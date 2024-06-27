610e80617d6326c8b99d994466fda4add757cbd9
Subhasish Nag <subhasish.nag@gmail.com>
2024-06-27 12:27:03 +0530

    initial commit

---
+++ Redme.md
@@ -0,0 +1,8 @@
+# This is multi module sample project with technologies
+1. spring-boot
+2. kafka
+3. avro
+4. database
+
+
+git remote add origin https://$GITHUB_TOKEN@github.com/subhasish747/spring-kafka-avro.git
---
+++ coffe-order-service/src/main/java/com/org/CoffeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeOrderApplication.class, args);
+	}
+
+}
